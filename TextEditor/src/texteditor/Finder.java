5c783ee42975fabe917aafb25560243512b4e4a7
DukeAniket <58697017+DukeAniket@users.noreply.github.com>
2021-10-17 14:05:34 +0530

    Create Finder.java

---
+++ TextEditor/src/texteditor/Finder.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package texteditor;
+
+import java.util.*;
+
+/**
+ *
+ * @author aniketkumar
+ */
+public class Finder {
+    
+    int wordlength;
+    int textlength;
+    String toFind;
+    String findFrom;
+    ArrayList<Integer> indexList;
+    
+    ArrayList<Integer> Finder(String toFind, String findFrom)
+    {
+        this.toFind = toFind;
+        this.findFrom = findFrom;
+        indexList = new ArrayList<>();
+        this.StringSearch();
+        return indexList;
+    }
+    
+    
+    private void StringSearch()
+    {
+        wordlength = this.toFind.length();
+        textlength = this.findFrom.length();
+        long hashWord = 0;
+        long hashText = 0;        
+        if(textlength < wordlength)
+        {
+            return;
+        }
+        
+        for(int i = 0; i < wordlength; i++)
+        {
+            hashWord = hashWord + (toFind.charAt(i));
+        }
+        
+        for(int i = 0; i < wordlength; i++)
+        {
+            hashText = hashText + (findFrom.charAt(i));
+        }
+        
+        for(int i = 1; i<=(textlength - wordlength);i++)
+        {
+            if(hashWord == hashText)
+            {
+                matchstring(i);
+            }
+            hashText = hashText - findFrom.charAt(i - 1);
+            hashText = hashText + findFrom.charAt(i + wordlength - 1);
+        }
+        if(hashWord == hashText)
+        {
+            matchstring(textlength - wordlength);
+        }
+    }
+    
+    private void matchstring(int i)
+    {
+        i--;
+        if(toFind.equals(findFrom.substring(i, i + wordlength)));
+        {
+            System.out.println(findFrom.substring(i, i + wordlength)+" , "+ findFrom.charAt(i + wordlength));
+            indexList.add(i);
+        }
+    }
+}
